2c8dcdc817ecc68a531d545f39b47de10dd69145
Hibbins <sebastian@westberg.io>
2024-11-17 21:34:15 +0100

    feat(src:utils.py): add more config vars
    Forgot one var in the last commit, the custom_string. Also added the new
    one for the new logic regarding no compositor mode.

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,11 @@ config.read(config_path)
 
 # Get the screenshot folder path
 screenshot_folder = os.path.expanduser(config.get("DEFAULT", "SCREENSHOT_FOLDER"))
+# Get the custom string to be appended to clipboard
+custom_string = config.get("DEFAULT", "CUSTOM_STRING")
 # Get the boolean value that determines if systray is displayed
 show_in_systray = config.getboolean("DEFAULT", "SHOW_IN_SYSTRAY")
 # Get the region coordinates from the latest selection
 selected_region_coordinates = config.get("DEFAULT", "SELECTED_REGION_COORDINATES", fallback="")
+# Get the boolean that determines wheter to run no compositor mode or not
+no_compositor_mode = config.getboolean("DEFAULT", "NO_COMPOSITOR_MODE")
