e18f951f386079a5de3d796539a1fa0dc7035ff3
Hibbins <sebastian@westberg.io>
2024-12-08 22:10:19 +0100

    feat(dumpmyscreen:__init__.py): init file for package

---
+++ dumpmyscreen/__init__.py
@@ -0,0 +1,7 @@
+from .version import __version__
+
+__author__ = "Sebastian Westberg"
+__license__ = "MIT"
+__maintainer__ = "Sebastian Westberg"
+__email__ = "sebastian@westberg.io"
+__version__ = __version__
